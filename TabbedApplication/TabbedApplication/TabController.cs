6a0ec1bcda34d23663ac42c9028841fc5cd9c0d5
David Kuehn <kuehnd96@gmail.com>
2017-07-11 22:10:27 -0500

    Created my first tabbed screen in iOS
    https://developer.xamarin.com/guides/ios/user_interface/controls/creatin
    g_tabbed_applications/

---
+++ TabbedApplication/TabbedApplication/TabController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UIKit;
+
+namespace TabbedApplication
+{
+    public class TabController : UITabBarController
+    {
+        UIViewController tab1, tab2, tab3;
+
+        public TabController()
+        {
+            tab1 = new UIViewController();
+            tab1.TabBarItem = new UITabBarItem(UITabBarSystemItem.Favorites, 0);
+            tab1.View.BackgroundColor = UIColor.Green;
+
+            tab2 = new UIViewController();
+            tab2.Title = "Orange";
+            tab2.View.BackgroundColor = UIColor.Orange;
+
+            tab3 = new UIViewController();
+            tab3.Title = "Red";
+            tab3.View.BackgroundColor = UIColor.Red;
+
+            ViewControllers = new UIViewController[] { tab1, tab2, tab3 };
+        }
+    }
+}
